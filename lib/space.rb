441d8faa95e2e4cd4f498f6b9d810bcfab4ba06e
Maria Eleni Tsirpanli <tsirpanlimariaeleni@gmail.com>
2021-04-07 15:00:24 +0100

    Unit tests for space class

--- lib/space.rb
+++ lib/space.rb
@@ -13,19 +13,19 @@ class Space
   end
 
   def self.create_space(name:, description:, price:, host:)
-    result = DatabaseConnection.query("INSERT INTO #{DatabaseConnection.dbname} (name, description, price, host) VALUES('#{name}', '#{description}', '#{price}') RETURNING id, name, description, price ")
+    result = DatabaseConnection.query("INSERT INTO spaces (name, description, price, host) VALUES('#{name}', '#{description}', '#{price}') RETURNING id, name, description, price ")
     Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host: result[0]['host'])
   end
 
-  def self.view_my_spaces(host:)
-    result = DatabaseConnection.query("SELECT * FROM #{DatabaseConnection.dbname} WHERE HOST = '#{host}';")
+  def self.view_my_spaces(host)
+    result = DatabaseConnection.query("SELECT * FROM spaces WHERE HOST = '#{host}';")
     result.map do |space| space
       Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host: result[0]['host'])
     end
   end
 
   def self.view_all_spaces()
-    result = DatabaseConnection.query("SELECT * FROM #{DatabaseConnection.dbname};")
+    result = DatabaseConnection.query("SELECT * FROM spaces;")
     result.map do |space| space
       Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host: result[0]['host'])
     end
