b135625d584b30b599f403d6cf1d3ea1ea76d9ef
Maria Eleni Tsirpanli <tsirpanlimariaeleni@gmail.com>
2021-04-07 12:07:07 +0100

    Database connection setup helper files

--- lib/space.rb
+++ lib/space.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require_relative 'database_connection'
 
 class Space
   attr_reader :name, :description, :price
@@ -12,32 +13,17 @@ class Space
   end
 
   def self.create_space(name:, description:, price:, host:)
-    #if ENV['ENVIRONMENT'] == 'test'
-      #connection = PG.connect(dbname: '#')
-    #else
-      #connection = PG.connect(dbname: '#')
-    #end
     result = connection.exec("INSERT INTO # (name, description, price, host) VALUES('#{name}', '#{description}', '#{price}') RETURNING id, name, description, price ")
     Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host: result[0]['host'])
   end
 
   def self.view_my_spaces(host:)
-    #if ENV['ENVIRONMENT'] == 'test'
-      #connection = PG.connect(dbname: '#')
-    #else
-      #connection = PG.connect(dbname: '#')
-    #end
     result = connection.exec("SELECT * FROM # WHERE HOST = '#{host}';")
     result.map do |space| space
       Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host: result[0]['host'])
     end
 
   def self.view_all_spaces()
-    #if ENV['ENVIRONMENT'] == 'test'
-      #connection = PG.connect(dbname: '#')
-    #else
-      #connection = PG.connect(dbname: '#')
-    #end
     result = connection.exec("SELECT * FROM #;")
     result.map do |space| space
       Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host: result[0]['host'])
