0f19e00b17e504ec0f78792bf18566a6a4e5e2fd
Maria Eleni Tsirpanli <tsirpanlimariaeleni@gmail.com>
2021-04-08 15:42:36 +0100

    Debugging space.rb to allow view all and my spaces to work

--- lib/space.rb
+++ lib/space.rb
@@ -12,22 +12,32 @@ class Space
     @host_id = host_id
   end
 
-  def self.create_space(name:, description:, price:, host_id:)
-    result = DatabaseConnection.query("INSERT INTO spaces (name, description, price, host_id) VALUES('#{name}', '#{description}', '#{price}', '#{host_id}') RETURNING id, name, description, price, host_id;")
-    Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host_id: result[0]['host_id'])
+  def self.create_space(name:, description:, price:)
+    result = DatabaseConnection.query("INSERT INTO spaces (name, description, price, host_id) VALUES('#{name}', '#{description}', '#{price}', '#{host_id}') RETURNING id, name, description, price ")
+    Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host: result[0]['host_id'])
   end
 
   def self.view_my_spaces(host_id:)
     result = DatabaseConnection.query("SELECT * FROM spaces WHERE host_id = '#{host_id}';")
     result.map do |space| space
-      Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host_id: result[0]['host_id'])
+      Space.new(
+        id: space['id'], 
+        name: space['name'], 
+        description: space['description'], 
+        price: space['price'], 
+        host_id: space['host_id'])
     end
   end
 
   def self.view_all_spaces()
     result = DatabaseConnection.query("SELECT * FROM spaces;")
     result.map do |space| space
-      Space.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], host_id: result[0]['host_id'])
+      Space.new(
+        id: space['id'], 
+        name: space['name'], 
+        description: space['description'], 
+        price: space['price'], 
+        host_id: space['host_id'])
     end
   end
 
