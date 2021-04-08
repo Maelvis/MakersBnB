217efa874010aa49c989ef036068d29371bcbdc4
Maria Eleni Tsirpanli <tsirpanlimariaeleni@gmail.com>
2021-04-08 17:12:30 +0100

    Small update on the name of the create function

--- lib/booking.rb
+++ lib/booking.rb
@@ -13,7 +13,7 @@ class Booking
     end
 
 
-    def self.request(space_id:, guest_id:, host_id:, start_date:, leave_date:)
+    def self.create(space_id:, guest_id:, host_id:, start_date:, leave_date:)
         result = DatabaseConnection.query("INSERT INTO bookings (space_id, guest_id, host_id, start_date, leave_date) VALUES('#{space_id}', '#{guest_id}', '#{host_id}', '#{start_date}', '#{leave_date}') RETURNING id, space_id, guest_id, host_id, start_date, leave_date;")
         Booking.new(id: result[0]['id'], space_id: result[0]['space_id'], guest_id: result[0]['guest_id'], host_id: result[0]['host_id'], start_date: result[0]['start_date'],leave_date: result[0]['leave_date'])
     end
